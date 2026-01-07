3f120b99964c63dddb72f2e7a16e7faedcb69391
Kabilan Sureshkumar <ktmkabilan2005@gmail.com>
2026-01-07 14:38:01 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (11.49%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int len = nums.length;
+        if(len == 0 ) return 0;
+        for(int i=0;i< len;i++){
+            if(nums[i] == target) return i;
+        }
+        return -1;
+    }
+}
