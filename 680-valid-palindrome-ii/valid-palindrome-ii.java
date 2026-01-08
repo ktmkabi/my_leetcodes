e4ba24d0a5f43d2b79e6b7ee76e040b1b3870f81
Kabilan Sureshkumar <ktmkabilan2005@gmail.com>
2026-01-08 15:30:30 +0530

    Time: 4 ms (99.68%) | Memory: 47.6 MB (84.95%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) == s.charAt(right)) {
+                left++;
+                right--;
+            } else {
+                // try skipping one character
+                return isPalindrome(s, left + 1, right) ||
+                       isPalindrome(s, left, right - 1);
+            }
+        }
+        return true;
+    }
+
+    // helper function
+    private boolean isPalindrome(String s, int l, int r) {
+        while (l < r) {
+            if (s.charAt(l) != s.charAt(r)) {
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
