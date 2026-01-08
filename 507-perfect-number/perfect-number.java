4b81139549f91a6bf9036333de6d6037be3bc1d0
Kabilan Sureshkumar <ktmkabilan2005@gmail.com>
2026-01-08 12:08:22 +0530

    Time: 2111 ms (19.86%) | Memory: 42.2 MB (44.33%) - LeetSync

---
+++ 507-perfect-number/perfect-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        int total = 0;
+        int temp = num;
+        for(int i=1;i<num;i++){
+            if(num%i == 0)total+=i;
+        }
+
+        return (total == temp);
+    }
+}
